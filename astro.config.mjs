b73adfbecd916b275d5aeb5055b588852a27b488
ZeStig <69384921+zstg@users.noreply.github.com>
2025-02-18 22:11:32 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://zstg.is-a.dev/",
-  base: "/",
+  // base: "/", DON'T SET THIS https://docs.astro.build/en/guides/deploy/github/#base
   trailingSlash: "always",
   integrations: [
     tailwind(
